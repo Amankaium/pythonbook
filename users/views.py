d564d67068d66914601ba40603ae93b345cd1a5e
Amankaium <kaium@list.ru>
2023-11-30 12:13:06 +0600

    init

---
+++ users/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+
+class UsersView(APIView):
+    def get(self, request, *args, **kwargs):
+        user_list = User.objects.all()
+        user_serializer = UserSerializer(user_list, many=True)
+        json_data = user_serializer.data
+        return Response(json_data)
