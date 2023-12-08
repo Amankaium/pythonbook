657ac6a4a93db98e97f1b8070f1648a2ce189b4d
К Аманбаев <kamanbaev@rp.lan>
2023-12-08 12:09:39 +0600

    added viewset

--- task/urls.py
+++ task/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from task.views.task import *
 from task.views.add_view import *
+from rest_framework import routers
 
 app_name = 'task'
+
+router = routers.DefaultRouter()
+router.register(r'task-viewset', TaskViewSet)
 
 urlpatterns = [
     path('detail/<int:pk>/', TaskDetailAPIView.as_view(), name="task-detail"),
@@ -12,4 +16,5 @@ urlpatterns = [
     path('list-generic/', TasksGenericView.as_view(), name='list-generic'),
 
     path('add-view/<int:task_pk>/', AddViewAPI.as_view(), name='add-view'),
+    path('task-vs/', include(router.urls)),
 ]
--- task/views/task.py
+++ task/views/task.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.generics import RetrieveAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.response import Response
+from rest_framework import viewsets
 from task.models import Task
 from task.serializers import *
 
@@ -35,5 +36,10 @@ class TaskGenericDetailAPIView(RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
 
 class TasksGenericView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskCreateSerializer
+
+
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskCreateSerializer
