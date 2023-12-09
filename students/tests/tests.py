c18c56e5236cd47aefe825027df3ec1df2d53bc2
Amankaium <kaium@list.ru>
2023-12-09 11:04:51 +0600

    fix tests

--- students/tests/tests.py
+++ students/tests/tests.py
@@ -5,6 +5,24 @@ from .factories import *
 
 class TestStudentList(TestCase):
     def test_student_list_should_success(self):
+        expected_data = [
+            {
+                'user': 2,
+                'phone_number': 'test phone number 0',
+                'programming_language': 'test prg lng 0'
+            },
+            {   
+                'user': 3,
+                'phone_number': 'test phone number 1',
+                'programming_language': 'test prg lng 1'
+            },
+            {   
+                'user': 4, 
+                'phone_number': 'test phone number 2',
+                'programming_language': 'test prg lng 2'
+            }
+        ]
+
         for i in range(3):
             student_obj = StudentFactory(
                 phone_number=f"test phone number {i}",
@@ -13,16 +31,14 @@ class TestStudentList(TestCase):
 
         user = UserFactory()
 
-        response = self.client.get(reverse_lazy("student:list"))        
+        response = self.client.get(reverse_lazy("students:list"))        
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "test student number 0")
-        self.assertContains(response, "test student number 1")
-        self.assertContains(response, "test student number 2")
+        self.assertEqual(response.json(), expected_data)
 
 
 class TestStudentDetail(TestCase):
     def test_one_student_via_factory_boy(self):
         student_obj = StudentFactory()
-        response = self.client.get(f'/api/student/detail/{student_obj.id}/')
+        response = self.client.get(f'/api/students/detail/{student_obj.id}/')
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, student_obj.phone_number)
