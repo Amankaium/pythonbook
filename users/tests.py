3f9c3da1b6b5863ac291428b5c3f8addb9f4ae38
Beishenbekovv <150322387+Beishenbekovv@users.noreply.github.com>
2023-12-11 11:22:13 +0600

    modified test

--- users/tests.py
+++ users/tests.py
@@ -1,13 +1,20 @@
 from django.test import TestCase
 from django.urls import reverse_lazy
-
+from django.contrib.auth.models import User
 
 class UserTestCase(TestCase):
     def test_get_users_list_should_success(self):
         response = self.client.get(reverse_lazy("usersapp:users"))
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data), User.objects.count())
 
 class MentorTestCase(TestCase):
     def test_get_users_list_should_success(self):
         response = self.client.get(reverse_lazy("usersapp:mentors"))
         self.assertEqual(response.status_code, 200)
+
+
+
+        
+
+
